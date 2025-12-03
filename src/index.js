9c825c50defe327bf8fa4e4e70a16ba1ad649d9a
Miza <mizaeron@gmail.com>
2025-12-03 21:21:31 +0000

    Start on circle functions

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import woman1 from "./woman1.jpeg";
 import woman2 from "./woman2.jpg";
 
 const pictureHolder = document.querySelector(".picture-frame");
+const firstMan = document.querySelector(".first-man");
+const secondMan = document.querySelector(".second-man");
+const firstCat = document.querySelector(".first-cat");
+const secondCat = document.querySelector(".second-cat");
+const firstWoman = document.querySelector(".first-woman");
+const secondWoman = document.querySelector(".second-woman");
 
 const catpic1 = document.createElement("img");
 catpic1.classList.add("cat1");
@@ -33,11 +39,6 @@ womanpic2.classList.add("woman2");
 womanpic2.src = woman2;
 
 pictureHolder.append(manpic1);
-// pictureHolder.append(manpic2);
-// pictureHolder.append(catpic1);
-// pictureHolder.append(catpic2);
-// pictureHolder.append(womanpic1);
-// pictureHolder.append(womanpic2);
 
 function nextSlide() {
   const rightArrow = document.querySelector(".arrow-right");
@@ -46,18 +47,28 @@ function nextSlide() {
     if (pictureHolder.contains(manpic1)) {
       manpic1.remove();
       pictureHolder.append(manpic2);
+      // secondMan.style.fill = "black";
+      // firstMan.style.fill = "white";
     } else if (pictureHolder.contains(manpic2)) {
       manpic2.remove();
       pictureHolder.append(catpic1);
+      // firstCat.style.fill = "black";
+      // secondMan.style.fill = "white";
     } else if (pictureHolder.contains(catpic1)) {
       catpic1.remove();
       pictureHolder.append(catpic2);
+      // secondCat.style.fill = "black";
+      // firstCat.style.fill = "white";
     } else if (pictureHolder.contains(catpic2)) {
       catpic2.remove();
       pictureHolder.append(womanpic1);
+      // secondCat.style.fill = "white";
+      // firstWoman.style.fill = "black";
     } else {
       womanpic1.remove();
       pictureHolder.append(womanpic2);
+      // firstWoman.style.fill = "white";
+      // secondWoman.style.fill = "black";
     }
   });
 }
@@ -84,6 +95,29 @@ function previousSlide() {
     }
   });
 }
+
+function checkImage() {
+  const images = pictureHolder.getElementsByTagName("img");
+
+  if (images.length === 0) {
+    console.log("No images present.");
+    return;
+  }
+
+  const currentImage = images[0];
+
+  if (currentImage.src.includes(manpic1.src)) {
+    console.log("manpic1");
+  } else if (currentImage.src.includes(manpic2.src)) {
+    console.log("manpic2");
+  }
+}
+
+const observer = new MutationObserver(() => {
+  checkImage();
+});
+
+observer.observe(pictureHolder, { childList: true, subtree: true });
 
 nextSlide();
 previousSlide();
